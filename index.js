9de2511dbd497696a547bda0621b0a0ca2101eaf
FarabyKhan <farabykhan346@gmail.com>
2026-02-25 10:20:32 +0600

    add loans patch for showOnHome toggle function

--- index.js
+++ index.js
@@ -4,15 +4,35 @@ const app = express()
 require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
-const port = process.env.port||3000
+const port = process.env.PORT ||3000
 
+const admin = require("firebase-admin");
+const serviceAccount = require("./loan-link-auth-firebase-adminsdk.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 // middleware
 app.use(express.json())
 app.use(cors());
 
- const verifyFBToken = (req, res, next) =>{
+ const verifyFBToken = async(req, res, next) =>{
   console.log('headers in the middleware', req.headers.authorization);
-  next()
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).send({ message: 'unauthorized access' })
+  }
+  try {
+    const idToken = token.split(' ')[1]
+    const decoded = await admin.auth().verifyIdToken(idToken)
+    console.log('decoded in the token', decoded);
+    req.user = decoded;
+    next()
+  } catch (error) {
+    return res.status(401).send( {message: 'unauthorize access'})
+  }
+  
  }
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@am.7mxwxuq.mongodb.net/?appName=AM`;
@@ -36,7 +56,22 @@ async function run() {
     const loansCollection = db.collection('loans');
     const loanApplicationCollection = db.collection('loanApply');
 
+    // Admin related middleware
+
+    const verifyAdmin = async(req,res,next)=>{
+      const email = req.user.email;
+      const query = { email };
+      const user = await usersCollection.findOne(query)
+
+      if (!user || user.role != 'admin') {
+        return res.status(403).send({message: 'forbidden access'})
+      }
+      next()
+    }
+
+
     // user api 
+
   app.post('/users', async(req,res)=>{
     const user = req.body
    const role = user.role
@@ -51,41 +86,109 @@ async function run() {
     res.send(result)
   })
 
-  app.get('/users/:email', async(req,res)=>{
+  app.get('/users', verifyFBToken, verifyAdmin, async(req,res)=>{
+    const cursor = usersCollection.find();
+    const result = await cursor.toArray()
+    res.send(result)
+  })
+
+  app.get('/users/:email', verifyFBToken, async(req,res)=>{
       const email = req.params.email
+      if (req.user.email !== email) {
+        return  
+      }
       const user = await usersCollection.findOne({email})
       res.send(user)
   })
 
+  app.patch('/users/role/:id', verifyFBToken, async(req,res)=>{
+    const reqUser =await usersCollection.findOne({
+      email:req.user.email
+    })
+    if(!reqUser || reqUser.role !== 'admin'){
+      return res.status(403).send({message:'forbidden access'})
+    }
+    const id = req.params.id
+    const{status, suspendReason} = req.body
 
-  app.post('/loans', async(req,res)=>{
-    const loan = req.body
-    const result = await loansCollection.insertOne(loan)
+    const targetUser = await usersCollection.findOne({
+      _id: new ObjectId(id)
+    })
+
+    if(targetUser.email === req.user.email){
+      return res.status(400).send({
+        message: 'You cannot modify your own account'
+      });
+    }
+
+    
+    const updateDoc ={
+      $set:{
+        status,
+        suspendReason: status === 'suspended' ? suspendReason : '',
+      }
+    }
+    const result = await usersCollection.updateOne(
+      {_id: new ObjectId(id)},
+      updateDoc
+    );
+
     res.send(result)
   })
 
 
-  app.get('/loans', async(req, res)=>{
+  app.post('/loans', verifyFBToken, verifyAdmin, async(req,res)=>{
+    const loan = req.body
+    const newLoan ={
+      ...loan,
+      createdBy: {
+        email:req.user.email,
+        name:req.user.name
+      },
+      createdAt: new Date(),
+    }
+    const result = await loansCollection.insertOne(newLoan)
+    res.send(result)
+  })
+
+
+  app.get('/loans', verifyFBToken, async(req, res)=>{
+    
     const cursor =loansCollection.find()
     const result = await cursor.toArray();
     res.send(result) 
   })
 
-  app.get('/loans/:id', verifyFBToken, async(req, res)=>{
+  app.get('/loans/:id', async(req, res)=>{
+    
     const id = req.params.id;
     const query = { _id: new ObjectId(id) }
     const result = await loansCollection.findOne(query)
     res.send(result)
   })
 
+  app.patch('/loans/show/:id', verifyAdmin,verifyFBToken, async(req,res)=>{
+    const id = req.params.id;
+    const {showOnHome} = req.body;
+
+    const result = await loansCollection.updateOne(
+      {_id: new ObjectId(id)},
+      {$set: { showOnHome }}
+    );
+    res.send(result)
+  });
+
   app.get('/featured-loans',async(req,res)=>{
-    const cursor = loansCollection.find().sort({ createdAt:-1 }).limit(6)
+    const cursor = loansCollection.find({ showOnHome:true }).sort({ createdAt:-1 }).limit(6)
     const result = await cursor.toArray();
     res.send(result)
 
   })
 
-  app.post('/loan-application',async(req,res)=>{
+  app.post('/loan-application',verifyFBToken, async(req,res)=>{
+    if(req.user.email !== req.body.email){
+      return res.status(401).send({message:'forbidden access'})
+    }
     const apply = req.body
     const result = await loanApplicationCollection.insertOne(apply)
     res.send(result)
